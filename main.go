15e608a867c17635d27e3fbb8c8cbfe754748ff7
Julio C Negrete <julio.negrete@kav.com.mx>
2020-12-16 19:33:37 -0600

    Se incluye conexion a la base de datos

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Jebus01/curso-rj-go-mongo/bd"
+	"github.com/Jebus01/curso-rj-go-mongo/handlers"
+)
+
 func main() {
 
+	if bd.VerificaConexionBD() == 0 {
+		log.Fatal("No hay conexion a la base de datos")
+		return
+	}
+
+	handlers.Manejadores()
 }
